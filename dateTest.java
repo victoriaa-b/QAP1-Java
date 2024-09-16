e0fc75ab283630a48751d14c37c0932e846c26bb
Victoria Breen <victoriabreen8@hotmail.com>
2024-09-16 19:14:36 -0230

    Testing for Date

---
+++ dateTest.java
@@ -0,0 +1,12 @@
+// A file to test the data in date.java
+
+public class dateTest {
+    public static void main(String[] args) {
+    // date object
+    // double check caps
+    date date = new date(16, 9, 2024);
+
+    // Display the date
+       System.out.println(date.toString());
+}
+}
